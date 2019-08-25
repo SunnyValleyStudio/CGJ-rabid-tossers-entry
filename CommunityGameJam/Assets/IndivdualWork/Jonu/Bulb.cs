fd74c6e933ef94ca146fa08e397a5217de02bcc2
TheJonu <the.jonu@gmail.com>
2019-08-25 14:35:51 +0200

    Added a ui progress bar

---
+++ CommunityGameJam/Assets/IndivdualWork/Jonu/Bulb.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Bulb : MonoBehaviour
+{
+
+    public Image lightImage;
+
+    void Start()
+    {
+        lightImage.enabled = false;
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void TurnOn()
+    {
+        lightImage.enabled = true;
+    }
+
+    public void TurnOff()
+    {
+        lightImage.enabled = false;
+    }
+}
---
+++ CommunityGameJam/Assets/IndivdualWork/Jonu/ProgressBar.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProgressBar : MonoBehaviour
+{
+    public GameObject bulbPrefab; // the prefabs to spawn
+    public Transform parent; // the parent for the bulbs
+
+    public int number; // how many lights there are
+
+    public float bulbWidth; // how wide one bulb is
+
+    GameObject[] bulbs;
+    Bulb[] scripts;
+
+    public int progress; // how many bulbs are on
+
+    void Start()
+    { 
+        bulbs = new GameObject[number];
+        scripts = new Bulb[number];
+
+        // spawn the bulbs
+        for(int i=0; i<number; i++)
+        {
+            bulbs[i] = Instantiate(bulbPrefab, parent);
+            bulbs[i].transform.localPosition = new Vector2(parent.position.x + i * bulbWidth, parent.position.y);
+
+            scripts[i] = bulbs[i].GetComponent<Bulb>();
+        }
+    }
+    
+    void Update()
+    {
+        //ChangeProgress(0);
+    }
+
+    // a change to progress, changing the amount of lit bulbs
+    public void ChangeProgress(int amount)
+    {
+        // apply the change
+        progress += amount;
+
+        // update the bulbs
+        for(int i = 0; i < number; i++)
+        {
+            if(i < progress)
+            {
+                scripts[i].TurnOn();
+            }
+            else
+            {
+                scripts[i].TurnOff();
+            }
+        }
+    }
+}
