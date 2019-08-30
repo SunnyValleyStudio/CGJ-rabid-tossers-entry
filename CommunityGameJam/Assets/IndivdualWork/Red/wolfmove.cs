9b77e43d3151ebd41f85b88ca9ac7e7d5786af3b
TheRedGamingEagle <54472840+TheRedGamingEagle@users.noreply.github.com>
2019-08-30 16:45:58 -0300

    added wolf

---
+++ CommunityGameJam/Assets/IndivdualWork/Red/wolfmove.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wolfmove : MonoBehaviour
+{
+    public float speed = 2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Input.GetAxis("horizontal"), 0, -Input.GetAxisRaw("Vertical")+Time.deltaTime*speed);
+       
+    }
+}
